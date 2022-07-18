768baad8fa8630e36060f8a8dcf1188c2338347f
Juan-Tafolla <juantafolla2001@gmail.com>
2022-07-18 15:06:53 -0500

    Successfully finished task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,33 @@
-const sumAll = function() {
+let result = 0;
+let a = 0;
+let b = 0;
+
+const sumAll = function sumAll(a,b) {
+
+        if (a > 0 &&  b > 0  && typeof a === 'number' && typeof b === 'number' ) {
+            
+            let i = 0;
+            if (a > b) {
+
+                result = a;
+
+                while (a > b){
+                    a --;
+                    result += a;
+                }
+            }else{
+
+                result = b;
+                while (b > a){
+                    b --;
+                    result += b;
+                }
+            }
+            return result;
+        }
+        else{
+            return 'ERROR';
+        }
 
 };
 
